9df42997c65e75657cb015aaf186ddcc6a733131
clevercoderjoy <clevercoderjoy.com>
2025-05-25 13:37:25 +0530

    routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom"
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <>
-      <h1 className="text-red-500 font-bold text-4xl">Hello</h1>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   )
 }
